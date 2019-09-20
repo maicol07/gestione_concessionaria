0374ed3e97df013d1e13e992da630c8b12cd25ae
Maicol <maicolbattistini@live.it>
2019-09-20 18:24:54 +0200

    Various improvements
    - Added comments
    - Added database connection
    - Many improvements and fixes

--- concessionaria.py
+++ concessionaria.py
@@ -1,23 +1,52 @@
+# Import tkinter, ttk e sqlite3
 from tkinter import *
 from tkinter.ttk import *
 import sqlite3 as sql
+
+# Apertura database (viene creato se non esiste il file) e creazione tabelle se non esistono già
+db = sql.connect("db.db", isolation_level=None)
+query = open("tables.sql")
+db.executescript(query.read())
+
 
 # Maicol
 def visualizzaMarca(marca):
     pass
 
+
 # Ale
 def selettoreMarche():
-    pass
+    w = Toplevel()
+    w.title("Gestione Concessionaria")
+    f = Frame(w)
+    f.pack()
+
+    w.mainloop()
+
 
 # Massa
 def visualizzaVeicolo(veicolo):
     pass
+    f = Toplevel()
+    f.title("Info Veicolo")
+    fr = Frame(f)
+
+    f.mainloop()
+
+
+### MAIN (Finestra principale)
 tk = Tk()
+# Impostazione titolo
 tk.title("Gestione Concessionaria")
+# Creazione variabile immagine
 logo = PhotoImage(file="logo.png")
+# Creazione etichetta immagine
 l = Label(tk, image=logo)
+# Impacchettamento etichetta (alla finestra)
 l.pack()
-btn = Button(tk, text="VAI AL SELETTORE DELLE MARCHE")
-btn.pack(pady=10)
+# Creazione pulsante
+btn = Button(tk, text="VAI AL SELETTORE DELLE MARCHE", command=selettoreMarche)
+# Impacchettamento pulsante (alla finestra)
+btn.pack(pady=20)
+# Inizio ciclo eventi
 tk.mainloop()
---
+++ tables.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS "marche" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"nome"	TEXT NOT NULL,
+	"logo"	TEXT
+);
+
+CREATE TABLE IF NOT EXISTS "veicoli" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"marca"	INTEGER,
+	"serie"	TEXT,
+	"modello"	TEXT,
+	"cavalli"	TEXT,
+	"anno_costruzione"	TEXT,
+	"categoria"	TEXT,
+	"prezzo"	REAL,
+	"qta"	INTEGER
+)
