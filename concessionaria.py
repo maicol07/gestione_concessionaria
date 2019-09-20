c394045d4d885d5086a2da0e0cf41c1b49de2d64
Maicol <maicolbattistini@live.it>
2019-09-20 17:33:45 +0200

    Initial update

---
+++ concessionaria.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter.ttk import *
+import sqlite3 as sql
+
+# Maicol
+def visualizzaMarca(marca):
+    pass
+
+# Ale
+def selettoreMarche():
+    pass
+
+# Massa
+def visualizzaVeicolo(veicolo):
+
+tk = Tk()
+tk.title("Gestione Concessionaria")
+logo = PhotoImage(file="logo.png")
+l = Label(tk, image=logo)
+l.pack()
+btn = Button(tk, text="VAI AL SELETTORE DELLE MARCHE")
+btn.pack(pady=10)
+tk.mainloop()
