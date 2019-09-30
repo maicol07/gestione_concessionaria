d58b782f4ccf8fd38ef72f89b6f7a7b76ce28a04
Maicol <maicolbattistini@live.it>
2019-09-30 17:08:33 +0200

    Fix PIL import (TTKTHEMES)
    Signed-off-by: Maicol <maicolbattistini@live.it>

--- lib/ttkthemes/_widget.py
+++ lib/ttkthemes/_widget.py
@@ -5,8 +5,40 @@ Copyright (c) 2017-2018 RedFantom
 """
 # Standard library
 import os
+import platform
+import sys
 import tkinter as tk
 from shutil import copytree, rmtree
+
+py_version = sys.version_info
+if py_version <= (3, 5):  # Verifico che la versione di Python installata sia superiore o uguale a 3.5 per il corretto
+    # funzionamento del programma. In caso negativo, lancio un messaggio di errore ed esco
+    tk.messagebox.showerror(title="Versione di Python non supportata",
+                            message="La versione di Python attualmente installata ({}.{}.{}) non è supportata. Aggiornare alla "
+                                    "versione 3.5 o successive".format(py_version.major, py_version.minor,
+                                                                       py_version.micro))
+    exit()
+
+system = platform.system().lower()
+if system != "linux":  # escludo linux in quanto ha già Pillow installato
+    if system == "windows":
+        if platform.architecture()[0] == "32bit":
+            os_info = "win32"
+        else:
+            os_info = "win-amd64"
+    else:
+        if py_version <= (3, 7):  # Se la versione di Python installata è minore della 3.7 su MAC il programma non
+            # funzionerà correttamente. Lancio un messaggio di errore ed esco.
+            tk.messagebox.showerror(title="Versione di Python non supportata",
+                                    message="È stato rilevato che il sistema in uso è MAC OS X. La versione di Python "
+                                            "attualmente installata ({}.{}.{}) non è supportata su questo tipo di sistema. "
+                                            "Aggiornare alla versione 3.7 o successive".format(py_version.major,
+                                                                                               py_version.minor,
+                                                                                               py_version.micro))
+            exit()
+        os_info = "macosx-10.14-x86_64"
+    sys.path.insert(0, os.path.abspath("lib/PIL/Pillow-6.1.0-py{}.{}-{}.egg".format(py_version.major, py_version.minor,
+                                                                                    os_info)))
 
 # Packages
 from PIL import Image, ImageEnhance
